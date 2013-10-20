f6ea27b42b36c37b5fdcdb176ed68d9a174e8c96
PurpleDizzy <PurpleDizzy@gmail.com>
2013-10-20 16:21:19 -0700

    (HUD) Updates and Cleanups

--- ares/gamemode/cl_hud.lua
+++ ares/gamemode/cl_hud.lua
@@ -7,8 +7,9 @@ end
 hook.Add("HUDShouldDraw","HUDHide",HUDHide)
 
 local colortable = {
-		rebel = Color( 210, 90, 0, 100),
-		imperial = Color( 0, 90, 210, 100)
+		rebel = 	team.GetColor (1),
+		imperial = 	team.GetColor (2),
+		spec = 	team.GetColor (1002)
 };
 
 
@@ -31,7 +32,6 @@ surface.CreateFont("WepFont", {font = "HUDNumber",
                                     weight = 700})
 -- Main --
 function AresHud()
-	DrawHPBarAndText()
 	CheckTeam()
 end	
 hook.Add( "HUDPaint", "AresHud", AresHud )
@@ -39,10 +39,12 @@ hook.Add( "HUDPaint", "AresHud", AresHud )
 function CheckTeam()
 	local Team = LocalPlayer():Team()
 	if Team == 1 then
+		DrawHPBarAndText()
 		RebelHUD()
 		DrawWEPBarAndText()
 	end
 	if Team == 2 then
+		DrawHPBarAndText()
 		ImperialHUD()
 		DrawWEPBarAndText()
 	end
@@ -53,26 +55,17 @@ end
 function RebelHUD()
 	local Team = "Rebel"
 	draw.RoundedBox( 4, 10, ScrH() - 100, 200, 40, colortable.rebel )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( 70, ScrH() - 99)
-	surface.SetFont( "TeamFont" )
-	surface.DrawText( Team )
+	draw.DrawText(Team, "TeamFont", 110, ScrH() - 99, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 end
 function ImperialHUD()
 	local Team = "Imperial"
 	draw.RoundedBox( 4, 10, ScrH() - 100, 200, 40, colortable.imperial )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( 50, ScrH() - 99)
-	surface.SetFont( "TeamFont" )
-	surface.DrawText( Team )
+	draw.DrawText(Team, "TeamFont", 110, ScrH() - 99, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 end
 function SpecHUD()
 	local Team = "Spectator"
-	draw.RoundedBox( 4, 10, ScrH() - 100, 200, 40, Color( 40, 40, 40, 100) )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( 40, ScrH() - 99)
-	surface.SetFont( "TeamFont" )
-	surface.DrawText( Team )
+	draw.RoundedBox( 4, 10, ScrH() - 50, 200, 40, colortable.spec )
+	draw.DrawText(Team, "TeamFont", 110, ScrH() - 50, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 end
 function DrawHPBarAndText()
 	local ply = LocalPlayer()
@@ -83,14 +76,8 @@ function DrawHPBarAndText()
 		draw.RoundedBox( 4, 10, ScrH() - 50, math.Clamp( HP, 0, 200 )*2, 40, Color( 220, 108, 108, 230) )
 		draw.RoundedBox( 4, 10, ScrH() - 50, math.Clamp( HP, 0, 200 )*2, 40, Color( 225, 255, 255, 40) )
 	end
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( 175, ScrH() - 40)
-	surface.SetFont( "HPFont" )
-	surface.DrawText( MHP )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( 20, ScrH() - 40)
-	surface.SetFont( "HPFont" )
-	surface.DrawText( "HP" )
+	draw.DrawText( MHP , "HPFont", 180, ScrH() - 40, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
+	draw.DrawText( "HP" , "HPFont", 25, ScrH() - 40, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 end
 
 function DrawWEPBarAndText()
@@ -98,48 +85,26 @@ function DrawWEPBarAndText()
 	local WEPN = LocalPlayer():GetActiveWeapon().PrintName
 	local TYPE = LocalPlayer():GetActiveWeapon().Type
 //	local SLOT = LocalPlayer():GetActiveWeapon().Slot
-//	local MAXC = LocalPlayer():GetActiveWeapon():GetTable().Primary.ClipSize
 if TYPE == "melee" then
-	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 100, 200, 40, Color( 100,100,100,100) )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( ScrW() - 155, ScrH() - 99)
-	surface.SetFont( "WepFont" )
-	surface.DrawText( WEPN )
-	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, 200, 40, Color( 40, 40 ,40, 50) )
-	//if CLIP ~= 0 then
-	//	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, math.Clamp( CLIP, 0, MAXC )*6.7, 40, Color(100,100,100,255) )
-	//	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, math.Clamp( CLIP, 0, MAXC )*6.7, 40, Color( 225, 255, 255, 40) )
-	//end
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( ScrW() - 150, ScrH() - 50)
-	surface.SetFont( "AmmoCFont" )
-	surface.DrawText( "Melee" )
+	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, 200, 40, Color( 100,100,100,100) )
+	draw.DrawText(WEPN, "WepFont", ScrW() - 115, ScrH() - 50, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 end
 if TYPE == "firearm" then
 	local WEP = LocalPlayer():GetActiveWeapon()
 	local CLIP = LocalPlayer():GetActiveWeapon():Clip1()
 	local AMMO = LocalPlayer():GetAmmoCount(WEP:GetPrimaryAmmoType())
+	local MAXC = LocalPlayer():GetActiveWeapon():GetTable().Primary.ClipSize
 	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 100, 200, 40, Color( 100,100,100,100) )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( ScrW() - 155, ScrH() - 99)
-	surface.SetFont( "WepFont" )
-	surface.DrawText( WEPN )
+	draw.DrawText(WEPN, "WepFont", ScrW() - 115, ScrH() - 99, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, 200, 40, Color( 40, 40 ,40, 50) )
-	//if CLIP ~= 0 then
-	//	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, math.Clamp( CLIP, 0, MAXC )*6.7, 40, Color(100,100,100,255) )
-	//	draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, math.Clamp( CLIP, 0, MAXC )*6.7, 40, Color( 225, 255, 255, 40) )
-	//end
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( ScrW() - 150, ScrH() - 50)
-	surface.SetFont( "AmmoCFont" )
-	surface.DrawText( CLIP )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( ScrW() - 100, ScrH() - 50)
-	surface.SetFont( "AmmoCFont" )
-	surface.DrawText( "/" )
-	surface.SetTextColor( 0, 0, 0, 255)
-	surface.SetTextPos ( ScrW() - 80, ScrH() - 50)
-	surface.SetFont( "AmmoCFont" )
-	surface.DrawText( AMMO )
+	if WEPN == "M4A1" then
+		if CLIP ~= 0 then
+			draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, math.Clamp( CLIP, 0, MAXC )*6.7, 40, Color(100,100,100,255) )
+			draw.RoundedBox( 4, ScrW() - 215, ScrH() - 50, math.Clamp( CLIP, 0, MAXC )*6.7, 40, Color( 225, 255, 255, 40) )
+		end
+	end
+	draw.DrawText( CLIP , "AmmoCFont", ScrW() - 150, ScrH() - 50, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
+	draw.DrawText( "/" , "AmmoCFont", ScrW() - 115, ScrH() - 52, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
+	draw.DrawText( AMMO , "AmmoCFont", ScrW() - 80, ScrH() - 50, Color( 0, 0, 0, 255),TEXT_ALIGN_CENTER)
 end
 end
